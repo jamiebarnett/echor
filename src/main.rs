8368d582f81d0f7b415d75c6c763905dab064841
Jamie Barnett <jamie.barnett@entura.co.uk>
2020-04-19 10:39:33 +0100

    added clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use clap::{Arg,App};
+
+struct Config {
+    port: String,
+    path: String
+}
+
+impl Config {
+
+}
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("echor")
+        .version("1.0")
+        .author("Jamie Barnett")
+        .about("will listen on a supplied path and port for any incoming requests and log them to stdout.")
+        .arg(Arg::with_name("path").short("p").long("path").help("set a path to handle request for. defaults to /").takes_value(true));
 }
